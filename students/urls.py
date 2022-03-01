756414838f187ee22a4e957eb151fc9f0cbcf682
uzairdurani <muhammaduzairdurani@gmail.com>
2022-03-01 07:25:33 -0800

    django orm new addition

--- students/urls.py
+++ students/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.Student_list, name='Student_list'),
+    path('not/', views.studentList, name='studentList'),
+    path('indi/', views.studentList2, name='studentList2'),
     path('search/<str:name>', views.searchName, name='Student_search')
+
 ]
--- students/views.py
+++ students/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Student
 from django.db import connection
+from django.db.models import Q
 # Create your views here.
 
 
@@ -12,6 +13,20 @@ def Student_list(request):
 
 
 def searchName(request, name):
-    print(name)
     posts = Student.objects.filter(surname__startswith=name)
+    return render(request, 'output.html', {'posts': posts})
+
+
+# not query
+def studentList(request):
+    # posts = Student.objects.exclude(age=12) & Student.objects.exclude(
+    #     firstname__startswith='Muhammed')
+    posts = Student.objects.filter(~Q(firstname__startswith='Muhammed'))
+    return render(request, 'output.html', {'posts': posts})
+
+
+# select individual fields
+def studentList2(request):
+    # posts = Student.objects.values('firstname', 'surname')
+    posts = Student.objects.filter(age=12).only('firstname', 'surname')
     return render(request, 'output.html', {'posts': posts})
